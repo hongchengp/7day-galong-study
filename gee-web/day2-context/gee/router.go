02a8014dc97932d0e9063e6dbf16b15bee143277
hcp <2560899500@qq.com>
2024-11-05 19:31:17 +0800

    路由功能，由 path 找到 pattern then 找到 handler

---
+++ gee-web/day2-context/gee/router.go
@@ -0,0 +1,27 @@
+package gee
+
+import "net/http"
+
+type router struct {
+	handlers map[string]HandlerFunc
+}
+
+func NewRouter() *router {
+	return &router{
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+func (router *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	router.handlers[key] = handler
+}
+
+func (router *router) handle(c *Context) {
+	key := c.Method + "-" + c.Path
+	if handler, ok := router.handlers[key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
